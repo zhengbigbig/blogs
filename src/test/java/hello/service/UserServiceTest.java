4803802dc7e9f6dad8ef0358b408ac9cb30c806b
zhengbigbig <780357902@qq.com>
2019-12-25 15:18:11 +0800

    引入Mokito 来Mock

---
+++ src/test/java/hello/service/UserServiceTest.java
@@ -0,0 +1,20 @@
+package hello.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findUserByUsername() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+}
