04c788a7bf99f0491a52a49957f9e63b772e3342
zhengbigbig <780357902@qq.com>
2019-12-31 17:34:16 +0800

    fix a bug

--- src/main/java/hello/dao/BlogDao.java
+++ src/main/java/hello/dao/BlogDao.java
@@ -1,9 +1,11 @@
 package hello.dao;
 
 import hello.entity.Blog;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BlogDao {
     public List<Blog> getBlogs(Integer page, Integer pageCount, Integer userId) {
         return null;
