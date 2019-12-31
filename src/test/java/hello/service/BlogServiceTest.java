3f9d4143f58d45f3814a181bb6577126556ce073
zhengbigbig <780357902@qq.com>
2019-12-31 15:57:35 +0800

    TDD 测试驱动开发

---
+++ src/test/java/hello/service/BlogServiceTest.java
@@ -0,0 +1,25 @@
+package hello.service;
+
+
+import hello.dao.BlogDao;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class BlogServiceTest {
+    @Mock
+    private BlogDao blogDao;
+    @InjectMocks
+    private BlogService blogService;
+
+    @Test
+    void testGetBlogs() {
+        blogService.getBlogs(1,1,1);
+        verify(blogDao).getBlogs(1,1,1);
+    }
+}
