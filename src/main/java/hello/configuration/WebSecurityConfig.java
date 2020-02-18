a2b2ac475803518eaf9a7161f28486932fe8aead
zhengbigbig <780357902@qq.com>
2020-02-18 16:17:35 +0800

    定义用户登录拦截和url拦截

--- src/main/java/hello/configuration/WebSecurityConfig.java
+++ src/main/java/hello/configuration/WebSecurityConfig.java
@@ -9,15 +9,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 import javax.inject.Inject;
 
 @Configuration
 @EnableWebSecurity
 // 告诉spring 加上web安全模块，设置后，所有的请求都会被拦截
+// 权限管理核心：需要实现AuthenticationManager、accessDecisionManager
+// 1.登陆验证拦截器AuthenticationProcessingFilter
+// 2.资源管理拦截器AbstractSecurityInterceptor
+/*
+ 用户登录 -> AuthenticationProcessingFilter拦截 ->
+ 调用 AuthenticationManager -> 调用ProviderManager进行用户验证
+ 验证通过后，将用户权限信息封装在User放在springboot全局缓存SecurityContextHolder
+
+ 访问url -> AbstractSecurityInterceptor拦截 ->
+ 调用FilterInvocationSecurityMetadataSource获取被拦截url所需权限 ->
+ 调用权限管理器 AccessDecisionManager 通过SecurityContextHolder获取用户权限
+ */
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Inject
     private UserDetailsService userService;
+    @Inject
+    private MyFilterSecurityInterceptor myFilterSecurityInterceptor;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,6 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .logout().permitAll();
+        http.addFilterBefore(myFilterSecurityInterceptor, FilterSecurityInterceptor.class);
     }
 
     // 鉴权
