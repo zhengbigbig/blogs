d9a69c1d3e7e454c4a909263171fffd1eae33e22
zhengbigbig <780357902@qq.com>
2020-02-26 22:07:43 +0800

    无缝迁移 mybatis-plus

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,6 +3,7 @@ package hello;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/hello/service/UserPlusService.java
@@ -0,0 +1,8 @@
+package hello.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import hello.entity.user.User;
+
+public interface UserPlusService extends IService<User> {
+
+}
